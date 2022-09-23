b9f3a20773cff12d3f1e72c26887ea44ac44fc74
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2022-09-23 15:39:55 -0400

    tmp

--- src/ChainlinkPriceOracle.sol
+++ src/ChainlinkPriceOracle.sol
@@ -7,31 +7,33 @@ import "solmate/auth/authorities/MultiRolesAuthority.sol";
 import "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./interfaces/IAdmin.sol";
 
+abstract contract ENS {
+    function resolver(bytes32 node) public virtual view returns (Resolver);
+}
+
+abstract contract Resolver {
+    function addr(bytes32 node) public virtual view returns (address);
+}
+
 /**
  * @notice This contract adapts the chainlink price oracle
  */
 contract ChainlinkPriceOracle is IPriceOracle {
-    AggregatorV3Interface public chainlinkPriceOracle;
-
-    constructor(address priceOracleAddress) {
-        chainlinkPriceOracle = AggregatorV3Interface(priceOracleAddress);
-    }
+    ENS ens = ENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
 
     /**
      * ///////////// IPriceOracleAdapter ////////////
      */
 
     /**
-     * @notice
+     * @notice Returns the price against USD for a specific ERC20, sourced from chainlink.
      * @param token The ERC20 token to retrieve the USD price for
-     * @return price The price of the token in USD
+     * @return price The price of the token in USD, scale The power of 10 by which the return is scaled
      */
-    function getPriceUSD(address token) external view returns (uint256) {
-        (, int256 price,,,) = chainlinkPriceOracle.latestRoundData();
-        // get rid of warnings
-        uint256 tmp = uint256(uint160(token));
-        uint256 price2 = uint256(price);
-        return tmp + price2;
+    function getPriceUSD(address token) external view returns (uint256 price, uint8 scale) {
+        address aggregator = _getAggregator(token);
+        (int256 rawPrice, scale) = _getPrice(aggregator);
+        price = uint256(rawPrice);
     }
 
     /**
@@ -40,5 +42,37 @@ contract ChainlinkPriceOracle is IPriceOracle {
      */
     function scale() external view returns (uint8) {
         return chainlinkPriceOracle.decimals();
+    }
+
+    /**
+     * @notice Internal resolver for getting token symbol .
+     * @param address ERC20 address.
+     * @return symbol Token symbol.
+     */
+    function getTokenSymbol(address token) public view returns(string symbol) {
+
+    }
+
+    /**
+    /////////// INTERNAL ////////////
+     */
+
+    function _getPrice(address aggregator) internal view returns (int256 price, uint8 decimals) {
+        (, int256 price,,,) = AggregatorV3Interface(aggregator).latestRoundData();
+        decimals = AggregatorV3Interface(aggregator).decimals();
+    }
+
+    function _getAggregator(address token) internal view returns (address aggregator) {
+
+    }
+
+    /**
+     * @notice Resolve ENS address to contract address.
+     * @param node The ENS node to resolve.
+     * @return The resolved contract address.
+     */
+    function resolve(bytes32 node) public view returns(address) {
+        Resolver resolver = ens.resolver(node);
+        return resolver.addr(node);
     }
 }
