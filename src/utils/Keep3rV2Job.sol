57ee2d2f71d37a8b0e5c879b486c6e9dbeaa6284
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2022-09-24 16:14:09 -0400

    extract admin fn into its own abstract class, separate from keep3r

--- src/utils/Keep3rV2Job.sol
+++ src/utils/Keep3rV2Job.sol
@@ -2,23 +2,18 @@
 pragma solidity 0.8.13;
 
 import "../interfaces/IKeep3rV2Job.sol";
+import "./Admin.sol";
 
 import "keep3r/solidity/interfaces/IKeep3r.sol";
 
-abstract contract Keep3rV2Job is IKeep3rV2Job {
+abstract contract Keep3rV2Job is IKeep3rV2Job, Admin {
     address public keep3r;
-    address internal admin;
 
     // taken from https://docs.keep3r.network/core/jobs#simple-keeper
     modifier validateAndPayKeeper(address _keeper) {
         _isValidKeeper(_keeper);
         _;
         IKeep3r(keep3r).worked(_keeper);
-    }
-
-    modifier requiresAdmin(address sender) {
-        require(sender == admin, "!ADMIN");
-        _;
     }
 
     function setKeep3r(address _keep3r) public requiresAdmin(msg.sender) {
